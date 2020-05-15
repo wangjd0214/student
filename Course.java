e881e04469fcce252ce99b1eae92f262db5890a1
pig <etoak@1911.com>
2020-05-15 11:18:22 +0800

    在家第四行加了注释

--- Course.java
+++ Course.java
@@ -1,6 +1,6 @@
 public class Course {
 	private Integer id;//pig添加了注释 //同事修改
-	private String Coursename;//pig 添加了注释
-	private String name;
+	private String Coursename;//pig 添加了注释 
+	private String name;//在家加了注释
 	private Integer sourse;//同事在不同行添加了注释
 }
